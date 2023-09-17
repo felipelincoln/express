7c303de1a08bb91c6edab4a1714a09ab96ee821c
Felipe Lincoln <mail.felipelincoln@gmail.com>
2023-09-17 15:33:23 -0300

    wip

--- server.ts
+++ server.ts
@@ -1,6 +1,7 @@
 import express from 'express';
 import cors from 'cors';
 import { Alchemy, Network } from 'alchemy-sdk';
+import { MongoClient } from 'mongodb';
 
 const app = express();
 
@@ -10,6 +11,11 @@ const alchemy = new Alchemy({
   apiKey: process.env.ALCHEMY_API_KEY,
   network: Network.ETH_MAINNET,
 });
+
+const mongoDbUri =
+  'mongodb+srv://express:unz3JN7zeo5rLK3J@free.ej7kjrx.mongodb.net/?retryWrites=true&w=majority&appName=AtlasApp';
+
+const client = new MongoClient(mongoDbUri);
 
 interface NFT {
   tokenId: string;
@@ -35,6 +41,13 @@ async function getNFTsFromAlchemy(contract: string, user: string, page?: string)
 
   return nfts;
 }
+
+app.get('/healthcheck', async (req, res) => {
+  await client.connect();
+  await client.db('test').command({ ping: 100 });
+  await client.close();
+  res.send('OK');
+});
 
 app.get('/nfts/:contract/:user', async (req, res) => {
   const { contract, user } = req.params;
