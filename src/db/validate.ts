2c2b421a24aac47a55cef2e4ca0d01c446185dc3
Felipe Lincoln <mail.felipelincoln@gmail.com>
2024-07-07 01:33:19 -0300

    allow order with no fee

--- src/db/validate.ts
+++ src/db/validate.ts
@@ -1,7 +1,7 @@
 import { DbOrder } from './types';
 
 export function isOrderValid(order: DbOrder): boolean {
-  if (!order.fee) return false;
+  //if (!order.fee) return false;
 
   if (!order.fulfillmentCriteria.coin && order.fulfillmentCriteria.token.amount == '0')
     return false;
